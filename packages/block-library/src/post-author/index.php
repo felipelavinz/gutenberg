e6a1d1ce96a194096ad3fe6ab14e0fc21369fc16
Andrew Duthie <andrew@andrewduthie.com>
2020-04-17 16:54:30 -0400

    Block Library: Update FSE blocks to use block context

--- packages/block-library/src/post-author/index.php
+++ packages/block-library/src/post-author/index.php
@@ -88,25 +88,27 @@ function post_author_build_css_font_sizes( $attributes ) {
 /**
  * Renders the `core/post-author` block on the server.
  *
- * @param  Object $block The block to render.
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
  * @return string Returns the rendered author block.
  */
-function render_block_core_post_author( $block ) {
-
-	$attributes = $block->attributes;
-
+function render_block_core_post_author( $attributes, $content, $block ) {
 	if ( empty( $attributes ) ) {
 		return '';
 	}
 
-	$post = gutenberg_get_post_from_context();
+	if ( ! isset( $block->context['postId'] ) ) {
+		return '';
+	}
 
-	if ( ! $post ) {
+	$author_id = get_post_field( 'post_author', $block->context['postId'] );
+	if ( empty( $author_id ) ) {
 		return '';
 	}
 
 	$avatar = ! empty( $attributes['avatarSize'] ) ? get_avatar(
-		$post->post_author,
+		$author_id,
 		$attributes['avatarSize']
 	) : null;
 
